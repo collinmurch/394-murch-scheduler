77171055e4d54513026e253f0a37de1352878e08
Collin Murch <collinmurch@horizon.local>
2021-12-31 16:52:41 -0800

    Filtered by Term

--- src/App.js
+++ src/App.js
@@ -19,6 +19,25 @@ const getCourseNumber = course => (
   course.id.slice(1, 4)
 ) 
 
+const TermButton = ({ term, setTerm, checked }) => (
+    <>
+        <input type="radio" id={term} checked={checked} className="btn-check" autoComplete="off" 
+            onChange={() => setTerm(term)}/>
+        <label class="btn btn-success m-1 p-2" htmlFor={term}>
+            { term }
+        </label>
+    </>
+)
+
+const TermSelector = ({ term, setTerm }) => (
+    <div className="btn-group">
+        { 
+            Object.values(terms)
+                .map(value => <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />)
+        }
+    </div>
+)
+
 const Course = ({ course }) => (
     <div className="card m-1 p-1">
         <div className="card-body">
@@ -28,11 +47,20 @@ const Course = ({ course }) => (
     </div>
 )
 
-const CourseList = ({ courses }) => (
-    <div className="course-list">
-        { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
-    </div>
-)
+const CourseList = ({ courses }) => {
+    const [term, setTerm] = useState('Fall')
+    const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course))
+
+    return (
+        <>
+            <TermSelector term={term} setTerm={setTerm} />
+
+            <div className="course-list">
+                { termCourses.map(course => <Course key={course.id} course={ course } />) }
+            </div>
+        </>
+    )
+}
 
 const App = () =>  {
     const [schedule, setSchedule] = useState();
