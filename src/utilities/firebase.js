3c5e2511fc7fb36f3d36edc524575691904bd589
collinmurch <collin@murch.net>
2022-01-02 18:23:25 -0800

    Removed keys from firebase.js

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -4,16 +4,13 @@ import { getAuth, GoogleAuthProvider, onIdTokenChanged, signInWithPopup, signOut
 
 import { getDatabase, onValue, ref, set } from 'firebase/database'
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBK17WGXejaGIc4zO0hW1wz-D7kreWJKUY",
-    authDomain: "murch-scheduler.firebaseapp.com",
-    databaseURL: "https://murch-scheduler-default-rtdb.firebaseio.com",
-    projectId: "murch-scheduler",
-    storageBucket: "murch-scheduler.appspot.com",
-    messagingSenderId: "47106337420",
-    appId: "1:47106337420:web:49d01305de5c550f21a9a9",
-    measurementId: "G-NJ0JRXCFJG"
-  }
+// Must include all required keys in env.json
+let firebaseConfig
+try {
+    firebaseConfig = require('../../env.json')
+} catch {
+    firebaseConfig = {}
+}
 
 const firebase = initializeApp(firebaseConfig)
 const database = getDatabase(firebase)
