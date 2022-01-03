90941dce622729101baf2622c19c9691fab9377c
collinmurch <collin@murch.net>
2022-01-02 17:43:18 -0800

    Fixed Authentication Problem

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -1,17 +1,18 @@
 import { useState, useEffect } from 'react'
 import { initializeApp } from 'firebase/app'
+
 import { getDatabase, onValue, ref } from 'firebase/database'
 
 const firebaseConfig = {
-    apiKey: process.env.API_KEY || "",
-    authDomain: process.env.AUTH_DOMAIN || "",
-    databaseURL: process.env.DATABASE_URL || "",
-    projectId: process.env.PROJECT_ID || "",
-    storageBucket: process.env.STORAGE_BUCKET || "",
-    messagingSenderId: process.env.MESSAGING_ID || "",
-    appId: process.env.APP_ID || "",
-    measurementId: process.env.MEASUREMENT_ID || ""
-}
+    apiKey: "AIzaSyBK17WGXejaGIc4zO0hW1wz-D7kreWJKUY",
+    authDomain: "murch-scheduler.firebaseapp.com",
+    databaseURL: "https://murch-scheduler-default-rtdb.firebaseio.com",
+    projectId: "murch-scheduler",
+    storageBucket: "murch-scheduler.appspot.com",
+    messagingSenderId: "47106337420",
+    appId: "1:47106337420:web:49d01305de5c550f21a9a9",
+    measurementId: "G-NJ0JRXCFJG"
+  }
 
 const firebase = initializeApp(firebaseConfig)
 const database = getDatabase(firebase)
